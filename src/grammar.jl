c50c23d66a8ba461e6f6cd3d806c85bd23023022
naelsondouglas <naelson17@gmail.com>
2018-10-16 01:26:10 -0300

    O first é calculado em todas produções sem dar erro

--- src/grammar.jl
+++ src/grammar.jl
@@ -13,9 +13,9 @@ FN_DCLR = [[DATA_TYPE,ID,O_BRCKT,PARAMS,C_BRCKT,O_C_BRCKT,:CMD,C_C_BRCKT]],
 # CMD -> CMD LOOP é recursivo à esquerda e CMD -> LOOP CMD vai gerar indecidibilidade com CMD -> LOOP
 CMD = [[:LOOP], [:ATRIB] , [:EXPR_ARIT] , [:EXPR_BOOL]],
 
-ATRIB = [[ID OPR_ATR CTN]],
+ATRIB = [[ID,OPR_ATR,CTN]],
 
-INT_DCLR = [[IDT_INT OPR_ATR CT_INT]],
+INT_DCLR = [[IDT_INT,OPR_ATR,CT_INT]],
 
 LOOP_WHILE = [[:BLK_WHILE, O_BRCKT, :EXPR_BOOL, C_BRCKT, O_C_BRCKT, :CMD, C_C_BRCKT]],
 LOOP_FOR = [[:BLK_FOR, O_C_BRCKT, INT_DCLR, COMMA, :EXPR_BOOL, COMMA, :CMD, C_C_BRCKT, O_C_BRCKT, :CMD, C_C_BRCKT]],
@@ -24,6 +24,7 @@ LOOP = [[:LOOP_FOR],[:LOOP_WHILE]],
 
 #TODO: Expandir definição
 EXPR_ARIT = [[CTN,:OPR_ARIT,:CTN]],
+OPR_LOGIC =[[OPR_ARIT]], #Todo: escrever os operadores lógicos
 EXPR_BOOL = [:EXPR_ARIT,:OPR_LOGIC,:EXPR_ARIT,:CTB],
 
 
@@ -35,7 +36,7 @@ EXP_ARIT = [[:CT_NUM,:OPR_ARIT,:CT_NUM],[:OPR_SUB,:CT_NUM,:OPR_ARIT,:CT_NUM],[O_
 
 #TODO: Expandir chaves
 OPR_ARIT = [[OPR_DM],[OPR_SUM],[OPR_SUB]],
-EXPR_ALG =  [[:EXPR_ARIT],[:TER_NUM, :OPR_ARIT, :EXPR_ALG]],
+EXPR_ALG =  [[:EXPR_ARIT],[:TERM_NUM, :OPR_ARIT, :EXPR_ALG]],
 
 #CHAR = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","w","z"," "],
 #NUM = ["0","1","2","3","4","5","6","7","8","9"],
@@ -49,7 +50,7 @@ OPRLR_EQ_DIF = [[OPRLR_EQ],[OPRLR_DIF]],
 OPRLR_LGT = [[OPRLR_LG,:OPRLR_LGTR]],
 OPRLR_LGTR = [[OPR_ATR],[EPISILON]],
 
-OPRLR_GT = [[">",:OPRLR_GTR]],
+OPRLR_G = [[OPRLR_GT,:OPRLR_GTR]],
 OPRLR_GTR= [[OPR_ATR],[EPISILON]],
 
 OPRLR = [[:OPRLR_LGT],[:OPRLR_GT],[OPRLR_EQ]],
