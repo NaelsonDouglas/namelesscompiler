36313c792ea8138e280dea183916ff7e97e47d28
luqsthunder <lucasthund3r@gmail.com>
2018-11-01 09:40:56 -0300

    new grammar

--- src/grammar_testing.jl
+++ src/grammar_testing.jl
@@ -1,5 +1,7 @@
 grammar =
 [
+
+"""
 ATRIB = [[[ID,OPR_ATR,CTN]],Int(ATRIB_)],
 CT_NUM = [[[CTN],[CT_FLOAT]],Int(CT_NUM_)],
 OPR_ARIT = [[[OPR_DM],[OPR_SUM],[OPR_SUB]],Int(OPR_ARIT_)],
@@ -32,4 +34,39 @@ OPRLR_LGT = [[[OPRLR_LG,:OPRLR_LGTR]],Int(OPRLR_LGT_)],
 OPRLR_G = [[[OPRLR_GT,:OPRLR_GTR]],Int(OPRLR_G_)],
 OPRLR = [[[:OPRLR_LGT],[OPRLR_GT],[OPRLR_EQ]],Int(OPRLR_)],
 FN_CALL = [[[ID,O_BRCKT, :PARAMS,C_BRCKT]],Int(FN_CALL_)],
+"""
+
+    S = [[[:TYPE,ID, :PARAM, O_C_BRCKT, :ALL_INTER, C_C_BRCKT]], Int(S_)],
+    TYPE = [[[IDT_INT], [IDT_CHAR], [IDT_FLOAT], [IDT_STRING]], Int(TYPE_)],
+    PARAM = [[[O_BRCKT, C_BRCKT], [O_BRCKT, :P1, C_BRCKT]], Int(PARAM_)],
+    P1 = [[[:TYPE, :IDVEC], [:TYPE, :IDVEC, COMMA, :P1]], , Int(P1_)],
+    IDVEC = [[[ID], [ID, VEC_IN, :EXPR_I_F]], Int(IDVEC_)],
+    ALL_INTER = [[[:RIF, :ALL_INTER], [ATTR , :ALL_INTER],
+                 [:RWHILE, :ALL_INTER], [:RFOR, :ALL_INTER]
+                 [:RCONT, :ALL_INTER]],Int(ALL_INTER_)],
+    RIF = [[[BLK_IF, O_BRCKT, :EXPR_BOOL, C_BRCKT, O_C_BRCKT, :ALL_INTER, C_C_BRCKT, :RIF1]], Int(RIF_)],
+    RIF1 = [[[BLK_ELSE, :RIF1], [BLK_ELSE, O_C_BRCKT, :ALL_INTER, C_C_BRCKT], [EPS]], Int(RIF1_)],
+    ATTR = [[[:TYPE, :IDVEC, EQ, :EXPR_I_F], [:TYPE, :IDVEC, EQ, EXPR_STR]], Int(ATTR_)],
+    RWHILE = [[[BLK_WHILE, O_BRCKT, :EXPR_BOOL, O_BRCKT, O_C_BRCKT, :ALL_INTER, C_C_BRCKT]], Int(RWHILE_)],
+    RFOR = [[[BLK_FOR, O_BRCKT, ATTR, COMMA,:EXPR_I_F, COMMA, :EXPR_I_F, C_BRCKT, O_C_BRCKT, :ALL_INTER,
+             C_C_BCKT]], Int(RFOR_)],
+    RCONT = [[[CONTINUE], [BREAK], [RETURN], [RETURN, EXPR_ALL]], Int(RCONT_)],
+    EXPR_ALL = [[[EXPR_I_F], [EXPR_STR], [EXPR_BOOL]], Int(EXPR_ALL_)],
+
+    EXPR_I_F =[[[:EX1] ,[:EX1, ,:EXPR_I_F]], Int(EXPR_I_F)],
+    EX1 = [[[:EX2,:EX11]], Int(EX1_)],
+    EX11 = [[[OPR_DM,EX2, :EX11],[EPS]], Int(EX11_)],
+    EX2 = [[[:IDVEC], [CT_FLOAT], [CT_INT],  [O_BRCKT, :EX2, C_BRCKT]], Int(EX2_)],
+
+
+    EXPR_BOOL = [[[EXB1] ,[:EXB1, OPRLR_OR, :EXPR_BOOL]], Int(EXPR_BOOL_)],
+    EXB1 = [[[:EXB1, :EXB11]], Int(EXB1_)],
+    EXB11 = [[[OPRLR_AND, :EXB2, :EXB11], [EPS]], Int(EXB11_)],
+    EXB2 = [[[:IDVEC], [C_BRCKT, :EXPR_BOOL, C_BRCKT] ,[CT_INT]], Int(EXB2_)]
+
+
+    EXPR_STR =[[EXS2,EXS1], Int(EXPR_STR_)]
+    EXS1 = [[[OPR_PS,EXS2, EXS1], [EPS]], Int(EXS1_)]
+    EXS2 = [[[IDVEC], [CT_STR]], Int(EXS2_)]
+
 ]
