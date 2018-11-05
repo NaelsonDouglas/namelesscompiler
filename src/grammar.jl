a12eae6faa4bce37fe70a192f28a0355eba9e787
luqsthunder <lucasthund3r@gmail.com>
2018-11-05 01:11:07 -0300

    grammar changes

--- src/grammar.jl
+++ src/grammar.jl
@@ -54,7 +54,7 @@ end
     S -> TYPE 'id' PARAM '{' ALL_INTER '}' S
       -> EPS
 =#
-addProduction(:S, [[:RETYPE,ID, :PARAM, O_C_BRCKT, :ALL_INTER, C_C_BRCKT, :S],
+apppddProduction(:S, [[:RETYPE,ID, :PARAM, O_C_BRCKT, :ALL_INTER, C_C_BRCKT, :S],
                    [EPS]])
 
 #=
